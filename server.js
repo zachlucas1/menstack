46ed245e8f357a5e27b1f6d9449d24eeb0c0bcc7
emmymb20 <59660489+emmymb20@users.noreply.github.com>
2021-12-10 13:23:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const finalRoutes = express.Router();
 const PORT = 4000;
+let final_model = require('./final.model');
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
+mongoose.connect('mongodb://127.0.0.1:27017/finals', { useNewUrlParser: true });
+const connection = mongoose.connection;
